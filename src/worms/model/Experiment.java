471d9374759db7e6cf325ead1dafa98b29c8f952
Sebastian Stoelen <sebastianstoelen@Sebastians-MacBook-Pro.local>
2014-04-23 13:16:43 +0200

    Test suite

--- src/worms/model/Experiment.java
+++ src/worms/model/Experiment.java
@@ -5,9 +5,7 @@ import java.util.*;
 public class Experiment {
 
 	public static void main(String[] args) {
-		int x = 8;
-		double root = Math.pow(x,1.0/3);
-		System.out.println(root);
+		System.out.println(Worm.isValidCoordinate(Double.NaN));
 		
 	}
 
